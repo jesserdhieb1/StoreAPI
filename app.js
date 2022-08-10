a9d02c8e15b15ef71e7000442dc376e9dc86af4d
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-10 20:16:42 +0200

    commit

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const connectDB = require('./db/connect')
 const notFoundMiddleware=require('./middleware/not-found')
 const errorHandlerMiddleware=require('./middleware/error-handler')
 
-app.use(notFoundMiddleware)
+
 app.use(errorHandlerMiddleware)
 app.use(express.json())
 
+app.get('/',(req,res)=>{
+    res.status(200).send("<h1>Store API</h1> <a href ='/api/v1/products'>send</a>")
+})
 
+
+app.use(notFoundMiddleware)
 const url = process.env.MONGO_URI
 const port = process.env.PORT || 5000
 
