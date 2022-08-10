afd8a969da4e61d3467cac0be8b2d790df321ea6
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-10 20:01:01 +0200

    commit

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('04 Store API')
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const connectDB = require('./db/connect')
+const notFoundMiddleware=require('./middleware/not-found')
+const errorHandlerMiddleware=require('./middleware/error-handler')
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+app.use(express.json())
+
+
+const url = process.env.MONGO_URI
+const port = process.env.PORT || 5000
+
+const connect= async ()=>{
+    try{
+        await connectDB(url)
+        app.listen(port,()=>{
+            console.log(`connected on port ${port}`)
+        })
+    }
+    catch (error){
+        console.log(error)
+    }
+}
+
+connect();
+
