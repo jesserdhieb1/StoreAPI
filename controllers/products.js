1a5f25ec38ccd1e36c89c7f0acb5cc83e3cded3b
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-11 19:37:39 +0200

    commit

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req,res)=>{
 }
 
 const getAllProducts = async (req,res)=>{
-    const {featured,company,name,sort} = req.query
+    const {featured,company,name,sort,field} = req.query
     const querryObject={}
     if (featured){
         querryObject.featured=featured==='true'?true:false
@@ -19,9 +19,20 @@ const getAllProducts = async (req,res)=>{
     }
     let result =  Product.find(querryObject)
     if (sort){
-        result.sort(sort)
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
     }
-    const products = await result
+    else{
+        result=result.sort('createdAt')
+    }
+    if (field){
+        const fieldList = field.split(',').join(' ')
+        result = result.select(fieldList)
+    }
+    const page= Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page-1)*limit
+    const products = await result.skip(skip).limit(limit)
     res.status(200).json({products,nbHits:products.length})
 }
 
